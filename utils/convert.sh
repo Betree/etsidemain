9c00251a64571f404894544fdb82cb94e52795f9
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-02-22 17:25:34 +1100

    Set new categories

--- utils/convert.sh
+++ utils/convert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Convert and truncate a video
 
-ffmpeg -i $1 -t 00:00:0$2 -async 1 -an converted_$1
+ffmpeg -i $1 -t 00:00:0$2 -async 1 -f mp4 -vcodec libx264 -an $1_converted.mp4
